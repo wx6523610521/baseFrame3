8d2779dce971e1cd989a926b4cc1db9d66ce2a77
cheng yonglin <wx6523610521@163.com>
2024-08-28 10:01:43 +0800

    init

--- base/src/main/java/work/chncyl/base/security/annotation/AnonymousAccess.java
+++ base/src/main/java/work/chncyl/base/security/annotation/AnonymousAccess.java
@@ -2,11 +2,7 @@ package work.chncyl.base.security.annotation;
 
 import java.lang.annotation.*;
 
-/**
- * 匿名接口注解，该注解标注的接口可以匿名访问
- */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface AnonymousAccess {
-}
+public @interface AnonymousAccess {}
